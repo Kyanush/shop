f6dbf2e7f22daa660af2236ba43b03dabababb9d
Куаныш <zheksenkulov.kuanysh@gmail.com>
2019-08-29 14:06:13 +0600

    29.08.2019

--- resources/js/components/products/Import.vue
+++ resources/js/components/products/Import.vue
@@ -2,57 +2,49 @@
     <div class="box">
 
         <div class="box-header with-border">
-            <button type="button" class="btn btn-danger pull-right">
-                <i class="fa fa-times" aria-hidden="true"></i>
-                Очистить
+            <button class="btn btn-primary btn-file">
+                <i class="fa fa-download" aria-hidden="true"></i> Загрузить Excel
+                <input id="readExcel" type="file" accept=".xlsx, .xls, .csv" @change="readExcel($event)">
             </button>
-
+            <button class="btn btn-primary" :disabled="!data.length" @click="send">
+                <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                Отправить
+            </button>
         </div>
 
         <div class="table-responsive">
-            <table class="table table-bordered table-sort" id="products-list">
+            <table class="table table-bordered">
                 <thead>
                     <tr>
-                        <th v-for="column in columns">
-                            {{ column.title }}
+                        <th></th>
+                        <th width="130" v-for="number in data_max_length">
                             <select class="form-control">
                                 <option></option>
-                                <option v-for="column2 in columns" :value="column2.column">
-                                    {{ column2.title }}
+                                <option v-for="column in columns" :value="column.column">
+                                    {{ column.title }}
                                 </option>
                             </select>
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td v-for="column in columns">132</td>
+                    <tr v-for="(item, index) in data">
+                        <td>
+                            <input type="checkbox" v-model="selected[ index ]"/>
+                        </td>
+                        <td width="130" v-for="number in data_max_length">
+                            {{ item[ number - 1 ] }}
+                        </td>
                     </tr>
                 </tbody>
-                <tfoot>
-                    <tr>
-                        <th v-for="column in columns">
-                            {{ column.title }}
-                        </th>
-                    </tr>
-                </tfoot>
             </table>
         </div>
-
-        <input type="file" id="input" @change="setValue($event)"/>
 
     </div>
 </template>
 
-
 <script>
-
-
-
     export default {
-        components:{
-
-        },
         data () {
             return {
                 columns:[
@@ -67,17 +59,61 @@
                     { column: 'active',           title: 'Статус(1-Активный, 0-Неактивный)' },
                     { column: 'youtube',          title: 'Youtube ссылка' },
                     { column: 'view_count',       title: 'Кол-во просмотров' },
-                ]
+                ],
+                data: [],
+                data_max_length: 0,
+                selected: []
             }
         },
         created() {
         },
         methods:{
-            setValue(event){
+            send(){
+
+            },
+            readExcel(event){
 
                 var oFile = event.target.files[0];
 
+                document.getElementById("readExcel").value = "";
 
+                var sFilename = oFile.name;
+
+                var reader = new FileReader();
+                var result = {};
+                var self = this;
+
+                reader.onload = function (e) {
+                    var data = e.target.result;
+                    data = new Uint8Array(data);
+                    var workbook = XLSX.read(data, {type: 'array'});
+                    console.log(workbook);
+                    var result = {};
+                    workbook.SheetNames.forEach(function (sheetName) {
+                        var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], {header: 1});
+                        if (roa.length) result[sheetName] = roa;
+                    });
+                    // see the result, caution: it works after reader event is done.
+
+                    self.data = result[ workbook.SheetNames ];
+                    self.data.forEach(function (item, index) {
+                        self.selected[index] = true;
+                        if(item.length > self.data_max_length)
+                            self.data_max_length = item.length;
+                    });
+
+                    for (var number = 0; number < self.data_max_length; number++)
+                    {
+                        self.data.forEach(function (item, index) {
+                            if(!self.data[ index ][ number ])
+                                 self.data[ index ][ number ] = '';
+                        });
+                    }
+
+                    console.log(self.data);
+
+                };
+                reader.readAsArrayBuffer(oFile);
 
             }
         }
@@ -85,4 +121,9 @@
 </script>
 
 <style scoped>
+    table {
+        max-height: 500px;
+        overflow: auto;
+        display:inline-block;
+    }
 </style>
